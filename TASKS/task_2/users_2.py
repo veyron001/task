e0dad2b59245b5fff4ce1d7946f284ff08bfbeab
veyron001 <105966618+veyron001@users.noreply.github.com>
2022-05-23 21:37:54 +0300

    Initial commit

---
+++ TASKS/task_2/users_2.py
@@ -0,0 +1,16 @@
+users = [{
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+}, {
+    "id": 2,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "email": "Julianne.OConner@kory.org",
+}, {
+    "id": 3,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "email": "Lucio_Hettinger@annie.ca",
+}]
